8ecf9fa13f7558f7dcd1e9f1e4787d222a1ef6e1
Andi Anwar <andianwarnwr@gmail.com>
2025-04-16 23:08:13 +0800

    django templates

--- andiSmash/views.py
+++ andiSmash/views.py
@@ -1,18 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-    template_name = 'home.html'
+    template_name = 'halaman/index.html'
     context = {
-        'title': 'my home',
-        'welcome': 'welcome to my home',
+        'title': '-HOMEPAGE-',
+        'description': 'web portfolio saya',
+        'body': 'Halaman home'
     }
     return render(request, template_name, context)
 
 def about(request):
     template_name = 'about.html'
     context = {
-        'title': 'about me',
-        'welcome': 'ini page about',
+        'title': '-ABOUT PAGE-',
+        'description': 'web portfolio saya',
+        'body': 'Halaman about'
+    }
+    return render(request, template_name, context)
+
+def contact(request):
+    template_name = 'contact.html'
+    context = {
+        'title': '- CONTACT -',
+        'description': 'Contact',
+        'body': 'Contact Page'
     }
     return render(request, template_name, context)
